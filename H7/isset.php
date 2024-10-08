0a3129f98d086d7a39c22890e3ab8f6e560bd38c
yazidilham <ilhamcxcx356@gmail.com>
2024-10-08 09:30:08 +0700

    Practical Section 1

---
+++ H7/isset.php
@@ -0,0 +1,8 @@
+<?php
+$data = array("nama" => "Jane", "usia" => 25);
+if (isset($data["nama"])) {
+    echo "Nama: " . $data["nama"];
+} else {
+    echo "Variabel 'nama' tidak ditemukan dalam array.";
+}
+?>

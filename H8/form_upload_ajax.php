84f3dfefa9f12bef50033b5a5f3da4d0276c6cb6
yazidilham <ilhamcxcx356@gmail.com>
2024-10-15 09:29:21 +0700

    Practical Section 3. Upload Files with PHP and Jquery

---
+++ H8/form_upload_ajax.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<link rel="stylesheet" type="text/css" href="upload.css">
+<title>Unggah File Dokumen</title>
+</head>
+
+<body>
+<div class="upload-form-container">
+<h2>Unggah File Dokumen</h2>
+<form id="upload-form" action="upload.php" method="post" enctype=
+"multipart/form-data">
+<div class="file-input-container">
+<input type="file" name="file" id="file" class="file-input">
+<label for="file" class="file-label">Pilih File</label>
+</div>
+<button type="submit" name="submit" class="upload-button" id="upload-button"
+disabled>Unggah</button>
+</form>
+<div id="status" class="upload-status"></div>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="upload.js"></script>
+</body>
+
+</html>
